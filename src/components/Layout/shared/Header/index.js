af551ae357b0a3677cd1946bfda985c9e3b97b5d
GiangMinhOfficial <ghieuming@gmail.com>
2023-09-09 19:35:31 +0700

    Xây dựng UI Header #4

--- src/components/Layout/shared/Header/index.js
+++ src/components/Layout/shared/Header/index.js
@@ -6,16 +6,36 @@ import {
   faSpinner,
   faMagnifyingGlass,
   faPlus,
+  faEllipsisVertical,
+  faLanguage,
+  faCircleQuestion,
+  faKeyboard,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
 import Button from '~/components/Button';
-import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { Wrapper as PopperWrapper, Menu } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faLanguage} />,
+    title: 'English',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    title: 'Feedback and help',
+    to: '/feedback',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: 'Keyboard shortcuts',
+  },
+];
 
 function Header() {
   const [searchResult, setSearchResult] = useState([]);
@@ -32,7 +52,7 @@ function Header() {
         <img src={images.logo} alt="Tiktok" />
 
         <Tippy
-          interactive={true}
+          interactive
           visible={searchResult.length > 0}
           render={(attrs) => (
             <div className={cx('search-result')} tabIndex="-1" {...attrs}>
@@ -65,7 +85,14 @@ function Header() {
           <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
             Upload
           </Button>
+
           <Button primary>Log in</Button>
+
+          <Menu items={MENU_ITEMS}>
+            <button className={cx('menu')}>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </button>
+          </Menu>
         </div>
       </div>
     </header>
